b892b07cb32c2cd3139d6b0bde7502824ecb2623
Maciej Tarnowski <maciej@tarnowski.it>
2014-12-27 23:39:42 +0100

    Hide a method

--- Shaper/Console.h
+++ Shaper/Console.h
@@ -11,10 +11,10 @@ public:
     int getWidth();
     int getHeight();
     void printChar(char character, int x, int y);
-    COORD getFontSize();
     float getFontRatio();
     void printHelp();
 
 private:
     void gotoXY(int x, int y);
+    COORD getFontSize();
 };
